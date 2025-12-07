5a45215781574a8f5d67747cf7f3a384ca30d973
hari6lakshman <hari.lakshman6@gmail.com>
2025-12-07 15:54:56 +0530

    Delete components directory

--- components/mio-logo.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/utils";
-
-export function MioLogo({ className }: { className?: string }) {
-  return (
-    <h1 className={cn("text-4xl font-bold font-headline text-primary tracking-widest antialiased", className)}>
-      Mio
-    </h1>
-  );
-}
